f81d2d675d6520a1b031b1a5bbd3fc7fb9e87b40
taonyzeng <taony.zeng@gmail.com>
2022-10-28 01:40:42 -0400

    created a plugin project.

---
+++ Source/PluginMaker/PluginMakerGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PluginMakerGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLUGINMAKER_API APluginMakerGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
